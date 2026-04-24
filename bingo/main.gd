e5c219e89ba7a71c1bb6b24f84b995795f6ef896
sander-lele <133796051+sander-lele@users.noreply.github.com>
2026-04-24 11:18:47 +0300

    Bingo 0.1

--- bingo/main.gd
+++ bingo/main.gd
@@ -1,64 +1,78 @@
 extends Control
 
+@export var NumberButton : Button
+@export var BallCounter : Label
+@export var NumbersLabel : Label
+
 var NumberScene = preload("res://number.tscn")
 
-var BArray : Array = []
-var IArray : Array = []
-var NArray : Array = []
-var GArray : Array = []
-var OArray : Array = []
+var BallCount : int = 48
+var DrawnNumber : Array = []
+#These are the numbers in each letter
+var BingoArray : Dictionary = {"B":[],"I":[],"N":[],"G":[],"O":[]} 
 
-var BArrayRange : Array = range(1,16)
-var IArrayRange : Array = range(16,31)
-var NArrayRange : Array = range(31,46)
-var GArrayRange : Array = range(46,61)
-var OArrayRange : Array = range(61,76)
+#These show the possible number range for each letter
+var BingoArrayRange : Dictionary = {"B":range(1,16),"I":range(16,31),"N":range(31,46),"G":range(46,61),"O":range(61,76)}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	create_board()
 	draw_board()
+	reset_range_array()
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
+	#Please please please remove later
 	if Input.is_action_just_pressed("debug"):
 		get_tree().reload_current_scene()
 
 func create_board():
-	BArrayRange.shuffle()
-	IArrayRange.shuffle()
-	NArrayRange.shuffle()
-	GArrayRange.shuffle()
-	OArrayRange.shuffle()
-	for i in range(5):
-		BArray.append(BArrayRange[0])
-		BArrayRange.pop_front()
-		IArray.append(IArrayRange[0])
-		IArrayRange.pop_front()
-		NArray.append(NArrayRange[0])
-		NArrayRange.pop_front()
-		GArray.append(GArrayRange[0])
-		GArrayRange.pop_front()
-		OArray.append(OArrayRange[0])
-		OArrayRange.pop_front()
+	#Shuffels the range array
+	for key in BingoArrayRange:
+		for i in 5:
+			BingoArrayRange[key].shuffle()
+			BingoArray[key].append(BingoArrayRange[key][0])
+			BingoArrayRange[key].pop_front()
+
+func reset_range_array():
+	BingoArrayRange = {"B":range(1,16),"I":range(16,31),"N":range(31,46),"G":range(46,61),"O":range(61,76)}
 
 func draw_board():
-	for i in BArray:
-		var NumberInstance = NumberScene.instantiate()
-		NumberInstance.number = i
-		$Board/B/VBoxContainer/NumberHolder.add_child(NumberInstance)
-	for i in IArray:
-		var NumberInstance = NumberScene.instantiate()
-		NumberInstance.number = i
-		$Board/I/VBoxContainer/NumberHolder.add_child(NumberInstance)
-	for i in NArray:
-		var NumberInstance = NumberScene.instantiate()
-		NumberInstance.number = i
-		$Board/N/VBoxContainer/NumberHolder.add_child(NumberInstance)
-	for i in GArray:
-		var NumberInstance = NumberScene.instantiate()
-		NumberInstance.number = i
-		$Board/G/VBoxContainer/NumberHolder.add_child(NumberInstance)
-	for i in OArray:
-		var NumberInstance = NumberScene.instantiate()
-		NumberInstance.number = i
-		$Board/O/VBoxContainer/NumberHolder.add_child(NumberInstance)
+	#Adds the number visuals
+	for key in BingoArray:
+		for i in 5:
+			var NumberInstance = NumberScene.instantiate()
+			NumberInstance.number = BingoArray[key][i]
+			get_node("Split/Board/"+str(key)+"/VBoxContainer/NumberHolder").add_child(NumberInstance)
+
+func Update_info_panel():
 	
+
+func pick_random_number():
+	var Num : int = randi_range(1,75)
+	var Result : String = "" 
+	if Num <= 15:
+		Result = "B"+str(Num)
+	elif Num <= 30:
+		Result = "I"+str(Num)
+	elif Num <= 45:
+		Result = "N"+str(Num)
+	elif Num <= 60:
+		Result = "G"+str(Num)
+	elif Num <= 75:
+		Result = "O"+str(Num)
+	return Result
+
+func check_value_on_board(BoardResult : String):
+	var BoardLetter : String = BoardResult[0]
+	var BoardNumber : int = int(BoardResult.substr(1))
+	DrawnNumber.append(BoardResult)
+	if BoardNumber in BingoArray[BoardLetter]:
+		print("yep")
+	else:
+		print("nah")
+
+func _on_button_pressed() -> void:
+	if BallCount >= 1:
+		BallCount -= 1
+		check_value_on_board(pick_random_number())
+	else:
+		$Split/InfoPanel/VBoxContainer/NumberButton.disabled = true
--- bingo/number.gd
+++ bingo/number.gd
@@ -1,4 +1,4 @@
-extends Panel
+extends Button
 
 var number : int = 0
 
