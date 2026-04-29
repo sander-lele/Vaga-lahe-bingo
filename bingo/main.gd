cd461be0303f6949d1b2c004aa120204164245cd
sander-lele <133796051+sander-lele@users.noreply.github.com>
2026-04-29 22:45:13 +0300

    Bingo 0.2

--- bingo/main.gd
+++ bingo/main.gd
@@ -8,6 +8,7 @@ var NumberScene = preload("res://number.tscn")
 
 var BallCount : int = 48
 var DrawnNumber : Array = []
+var PossibleDraws : Array = range(1,76)
 #These are the numbers in each letter
 var BingoArray : Dictionary = {"B":[],"I":[],"N":[],"G":[],"O":[]} 
 
@@ -17,7 +18,29 @@ var BingoArrayRange : Dictionary = {"B":range(1,16),"I":range(16,31),"N":range(3
 func _ready() -> void:
 	create_board()
 	draw_board()
-	reset_range_array()
+	PossibleDraws.shuffle()
+	#connects the main script with all the board buttons.
+	var buttons = get_tree().get_nodes_in_group("button")
+	for i in buttons.size():
+		if buttons[i].is_connected("pressed", Callable(self,"button_pressed")) == false or buttons[i].is_connected("mouse_entered", Callable(self,"button_hoverd")) == false:
+			buttons[i].pressed.connect(button_pressed.bind(buttons[i]))
+			buttons[i].mouse_entered.connect(button_hoverd.bind(buttons[i]))
+
+func button_pressed(inst : Button):
+	#$pressed.play()
+	if DrawnNumber.has(inst.number):
+		inst.disabled = true
+		check_game_state()
+
+func button_hoverd(inst):
+	if inst.disabled == false:
+		#$hover.play()
+		pass
+
+func check_game_state():
+	if DrawnNumber.count() <= 33:
+		#Checks for a corner game
+		check_board_space("B",0)
 
 func _physics_process(_delta: float) -> void:
 	#Please please please remove later
@@ -44,10 +67,15 @@ func draw_board():
 			get_node("Split/Board/"+str(key)+"/VBoxContainer/NumberHolder").add_child(NumberInstance)
 
 func Update_info_panel():
-	
+	$Split/InfoPanel/VBoxContainer/NumbersPanel.text = str(DrawnNumber)
 
-func pick_random_number():
-	var Num : int = randi_range(1,75)
+func check_board_space(Letter : string, Index : int):
+	if BingoArray[Letter][Index] ==:
+		return true
+	else:
+		return false
+
+func convert_number_to_bingo_ball(Num : int):
 	var Result : String = "" 
 	if Num <= 15:
 		Result = "B"+str(Num)
@@ -61,18 +89,12 @@ func pick_random_number():
 		Result = "O"+str(Num)
 	return Result
 
-func check_value_on_board(BoardResult : String):
-	var BoardLetter : String = BoardResult[0]
-	var BoardNumber : int = int(BoardResult.substr(1))
-	DrawnNumber.append(BoardResult)
-	if BoardNumber in BingoArray[BoardLetter]:
-		print("yep")
-	else:
-		print("nah")
-
-func _on_button_pressed() -> void:
+func _on_number_button_pressed() -> void:
+	#gets called when a ball is drawn
 	if BallCount >= 1:
 		BallCount -= 1
-		check_value_on_board(pick_random_number())
+		DrawnNumber.append(PossibleDraws[0])
+		PossibleDraws.pop_front()
+		Update_info_panel()
 	else:
 		$Split/InfoPanel/VBoxContainer/NumberButton.disabled = true
